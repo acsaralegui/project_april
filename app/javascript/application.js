0b10733369508c12e4305730d3fea4e4119148eb
Aaron Saralegui <aaron.saralegui@gmail.com>
2023-08-13 08:09:13 -0700

    application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,16 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 
+//= require rails-ujs
+//= require jquery3
+//= require activestorage
+//= require jquery
+//= require jquery.flexslider-min
+//= require jquery_ujs
+//= require turbolinks
+//= require popper
+//= require bootstrap
+//= require tether
+//= require bootstrap-sprockets.
+
+import * as bootstrap from "bootstrap"
