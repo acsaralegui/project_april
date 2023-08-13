4b48a52c954d039543e9ee3d22f2f936664ffac7
Aaron Saralegui <aaron.saralegui@gmail.com>
2023-08-12 18:59:12 -0700

    change application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,7 @@ const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
+config.assets.debug = false
 window.Stimulus   = application
 
 export { application }
